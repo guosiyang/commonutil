89945a1709916ca2f0cc8fed244b421e7257e1c1
zkrliuchao <1050570758@qq.com>
2019-12-20 16:40:20 +0800

    阿萨

---
+++ src/main/java/com/guosiyang/interfaces/OperateMapDataAble.java
@@ -0,0 +1,66 @@
+package com.guosiyang.interfaces;
+
+/**
+ * Created by admin on 2019/12/20.
+ */
+
+import com.guosiyang.constant.codeEnum.CodeList.TrueFlagEnum;
+
+/**
+ * @program: commonutil
+ * @className: OperateDataAble
+ * @version: 1.0
+ * @author: 郭思洋
+ * @date: 2019-12-15
+ * @sign: 熙熙攘攘, 人来人往
+ * @description: 对数据操作提供相关接口
+ */
+public interface OperateMapDataAble<K,V> extends OperatorAble {
+    /**
+     * @author 郭思洋
+     * @date 2019-12-08
+     * @description 提供查询并返回接口(针对映射查询)
+     */
+    public abstract V query(K key);
+
+    /**
+     * @author 郭思洋
+     * @date 2019-12-08
+     * @description 提供查询是否存在接口 TRUE 为存在 FLASE为不存在
+     */
+    public abstract TrueFlagEnum exist(K key);
+
+    /**
+     * @author 郭思洋
+     * @date 2019-12-08
+     * @description 提供更新接口并进行返回是否正确更新
+     */
+    public abstract TrueFlagEnum update(K key,V value);
+
+    /**
+     * @author 郭思洋
+     * @date 2019-12-08
+     * @description 提供新增接口
+     */
+    public abstract TrueFlagEnum add(K key,V value);
+
+    /**
+     * @author 郭思洋
+     * @date 2019-12-08
+     * @description 提供新增接口
+     */
+    public abstract TrueFlagEnum delete(K key,V value);
+
+//    /**
+//     * @author 郭思洋
+//     * @date 2019-12-08
+//     * @description 提供默认方法进行可以对实现该接口的传入对应的操作符进行操作
+//     */
+//    public  static <K,V> Object  operateData(OperateMapDataAble<K,V> operateMapDataAble,String operate,Object...mehtodParams){
+//        if (operateMapDataAble ==null || operate==null || "".equals(operate))
+//            throw new NullPointerException("传入参数为空");
+//        Class<OperateMapDataAble<K,V>> operateMapDataAbleClass= (Class<OperateMapDataAble<K, V>>) operateMapDataAble.getClass();
+//        operateMapDataAbleClass
+//
+//    }
+}
--- src/main/java/com/guosiyang/regex/RegexSingle.java
+++ src/main/java/com/guosiyang/regex/RegexSingle.java
@@ -1,5 +1,8 @@
 package com.guosiyang.regex;
 
+import com.guosiyang.constant.codeEnum.CodeList.TrueFlagEnum;
+import com.guosiyang.interfaces.OperateMapDataAble;
+
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -35,6 +38,61 @@ public class RegexSingle {
     }
 
     /**
+     * @program: commonutil
+     * @className: RegexSingle
+     * @version: 1.0
+     * @author: 郭思洋
+     * @date: 2019-12-08
+     * @sign: 熙熙攘攘, 人来人往
+     * @description: 创建内部类 对外暴露内部类 该内部类主要是提供相关对数据增删改查操作
+     * 注意 : 该数据类型不允许进行删除或者进行更新 调用会进行报错
+     */
+    class RegexSingleOperator implements OperateMapDataAble<String,Matcher>{
+        AbstractMap<String,Matcher> dataMap=null;
+        //当第一次被被调用时自动绑定到RegexSingle中的数据存储集合
+        {
+            dataMap = RegexSingle.this.stringToMathcer;
+        }
+        @Override
+        public Matcher query(String key) {
+            if (TrueFlagEnum.FALSE==this.exist(key)){
+                return null;
+            }else{
+                return dataMap.get(key);
+            }
+        }
+
+        @Override
+        public TrueFlagEnum exist(String key) {
+            if (key==null || "".equals(key))
+                return TrueFlagEnum.FALSE;
+            if (dataMap.containsKey(key)){
+                return TrueFlagEnum.TRUE;
+            }
+            return TrueFlagEnum.FALSE;
+        }
+
+        @Override
+        public TrueFlagEnum update(String key, Matcher value) {
+            return TrueFlagEnum.FALSE;
+        }
+
+        @Override
+        public TrueFlagEnum add(String key, Matcher value) {
+            if (key==null || value==null || "".equals(key))
+                return TrueFlagEnum.FALSE;
+            if (dataMap.containsKey(key))
+                return TrueFlagEnum.TRUE;
+            dataMap.put(key,value);
+            return TrueFlagEnum.TRUE;
+        }
+
+        @Override
+        public TrueFlagEnum delete(String key, Matcher value) {
+            return TrueFlagEnum.FALSE;
+        }
+    }
+    /**
      * @author 郭思洋
      * @sign 熙熙攘攘,人来人往
      * @date 2019-12-17
@@ -44,7 +102,7 @@ public class RegexSingle {
      * @since
      * @description 单例获取regexsingle对象
     */
-     static  RegexSingle getRegexSingle(){
+     private static  RegexSingle getRegexSingle(){
         if (regexSingle==null){
             synchronized (RegexSingle.class){
                 if (regexSingle==null){
@@ -53,5 +111,9 @@ public class RegexSingle {
             }
         }
         return regexSingle;
+    }
+
+    public static OperateMapDataAble<String,Matcher> getOperatorData(){
+         return new RegexSingleOperator();
     }
 }
